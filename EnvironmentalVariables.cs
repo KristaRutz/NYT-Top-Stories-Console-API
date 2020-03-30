0206783b7f27af030b546145cc8aa29a388dcf8e
Krista Rutz <rutzkri000@gmail.com>
2020-03-30 08:15:08 -0700

    create a basic console API app to return NYT top stories

---
+++ EnvironmentalVariables.cs
@@ -0,0 +1,7 @@
+namespace ApiTest
+{
+  public static class EnvironmentVariables
+  {
+    public static string ApiKey = "upQjcFnxMa2qkpBMJnfKaOGWIOMG8NSn";
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ApiTest
+{
+  class Program
+  {
+    public class Article
+    {
+      public string Section { get; set; }
+      public string Title { get; set; }
+      public string Abstract { get; set; }
+      public string Url { get; set; }
+      public string Byline { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+      var apiCallTask = ApiHelper.ApiCall(EnvironmentVariables.ApiKey);
+      var result = apiCallTask.Result;
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Console.WriteLine(jsonResponse["results"]);
+    }
+
+    class ApiHelper
+    {
+      public static async Task<string> ApiCall(string apiKey)
+      {
+        RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+        RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+        var response = await client.ExecuteTaskAsync(request);
+        return response.Content;
+      }
+    }
+  }
+}
